0eeac99899e5cb8bd59543b72799f8cd59f24d6f
Angelo <atap9000@outlook.com>
2023-12-02 18:52:28 -0400

    Advent of code 2023 first commit day 1 and 2

---
+++ 2023/Day1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023Day1
+{
+    internal class Program
+    {
+        const string TheSoCalledDocument = "AdventOfCode2023Day1.txt";
+
+        enum Numbers
+        {
+            zero = 0,
+            one = 1,
+            two = 2,
+            three = 3,
+            four = 4,
+            five = 5,
+            six = 6,
+            seven = 7,
+            eight = 8,
+            nine = 9
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of code 2023 - Day 1: Calibration Document");
+            string? currentExecutingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (string.IsNullOrWhiteSpace(currentExecutingPath))
+            {
+                Console.WriteLine("What the spruce?\n where is the current executing path then!?\n bruh i dont know lol.");
+                return;
+            }
+
+            string documentPath = Path.Combine(currentExecutingPath!, TheSoCalledDocument);
+            if (!File.Exists(documentPath))
+            {
+                Console.WriteLine("Where is the document HUH!?\n PUT IT OR ELSE...\n i will cry about it k.\n shutting down btw.");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(documentPath);
+            Regex rx = new(@"(zero|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            int result = 0;
+
+            StringBuilder stringBuilder = new();
+            foreach (var line in lines)
+            {
+                bool firstHasBeenFound = false;
+                string auxLine = BigBrainCleaning(line);
+                MatchCollection matches = rx.Matches(auxLine.Trim());
+                foreach (Match match in matches)
+                {
+                    if (!firstHasBeenFound)
+                    {
+                        auxLine = auxLine.Replace(match.Value, ((int)Enum.Parse(typeof(Numbers), match.Value, true)).ToString());
+                        firstHasBeenFound = true;
+                    }
+                    else
+                    {
+                        auxLine = auxLine.Replace(matches[^1].Value, ((int)Enum.Parse(typeof(Numbers), matches[^1].Value, true)).ToString());
+                        break;
+                    }
+                }
+                string first = auxLine.First(finalDigit => char.IsDigit(finalDigit)).ToString();
+                string last = auxLine.Last(finalDigit => char.IsDigit(finalDigit)).ToString();
+                result += int.Parse(first + last);
+                stringBuilder.Append(auxLine + "\n");
+            }
+            Console.WriteLine(stringBuilder.ToString());
+            Console.WriteLine("Required Calibration: " + result);
+        }
+
+        public static string BigBrainCleaning(string line)
+        {
+            Regex rx = new(@"(oneight|twone|threeight|sevenine|eightwo|eighthree|nineight)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(line.Trim());
+            foreach (Match match in matches)
+            {
+                switch (match.Value)
+                {
+                    case "oneight":
+                        line = line.Replace(match.Value, "oneeight");
+                        break;
+                    case "twone":
+                        line = line.Replace(match.Value, "twoone");
+                        break;
+                    case "threeight":
+                        line = line.Replace(match.Value, "threeeight");
+                        break;
+                    case "sevenine":
+                        line = line.Replace(match.Value, "sevennine");
+                        break;
+                    case "eightwo":
+                        line = line.Replace(match.Value, "eighttwo");
+                        break;
+                    case "eighthree":
+                        line = line.Replace(match.Value, "eightthree");
+                        break;
+                    case "nineight":
+                        line = line.Replace(match.Value, "nineeight");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return line;
+        }
+    }
+}
---
+++ 2023/Day2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection;
+
+namespace AdventOfCodeDay2
+{
+    internal class Program
+    {
+        const string TheSoCalledDocument = "AdventOfCodeDay2Gems.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of code 2023 - Day 2: Gem Games");
+            string? currentExecutingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (string.IsNullOrWhiteSpace(currentExecutingPath))
+            {
+                Console.WriteLine("What the spruce?\n where is the current executing path then!?\n bruh i dont know lol.");
+                return;
+            }
+
+            string documentPath = Path.Combine(currentExecutingPath!, TheSoCalledDocument);
+            if (!File.Exists(documentPath))
+            {
+                Console.WriteLine("Where is the document HUH!?\n PUT IT OR ELSE...\n i will cry about it k.\n shutting down btw.");
+                return;
+            }
+            FirstPart(documentPath);
+            SecondPart(documentPath);
+        }
+
+        public static void SecondPart(string documentPath)
+        {
+            string[] lines = File.ReadAllLines(documentPath);
+            List<int> totalValuePerGame = [];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string auxLine = lines[i].Trim().Replace(';', ',');
+                string[] gameValues = auxLine.Split(':');
+                string[] gemsShown = gameValues[1].Split(",");
+                int currentMaximumRed = 0;
+                int currentMaximumBlue = 0;
+                int currentMaximumGreen = 0;
+                foreach (var gems in gemsShown)
+                {
+                    string auxGems = gems.Trim();
+                    string[] values = auxGems.Split(" ");
+                    short amountOfGems = short.Parse(values[0]);
+                    string colorOfGem = values[1];
+                    switch (colorOfGem)
+                    {
+                        case "red":
+                            if (currentMaximumRed == 0 || amountOfGems > currentMaximumRed)
+                                currentMaximumRed = amountOfGems;
+                            break;
+                        case "blue":
+                            if (currentMaximumBlue == 0 || amountOfGems > currentMaximumBlue)
+                                currentMaximumBlue = amountOfGems;
+                            break;
+                        case "green":
+                            if (currentMaximumGreen == 0 || amountOfGems > currentMaximumGreen)
+                                currentMaximumGreen = amountOfGems;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                int sumOfGems = currentMaximumRed * currentMaximumBlue * currentMaximumGreen;
+                totalValuePerGame.Add(sumOfGems);
+            }
+            Console.WriteLine("Total Sum Part 2:" + totalValuePerGame.Sum());
+        }
+
+        public static void FirstPart(string documentPath)
+        {
+            string[] lines = File.ReadAllLines(documentPath);
+            List<int> possibleGames = Enumerable.Range(1, lines.Length).ToList();
+            List<int> impossibleGames = [];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                bool isGameImpossible = false;
+                string auxLine = lines[i].Trim().Replace(';', ',');
+                string[] gameValues = auxLine.Split(':');
+                string[] gemsShown = gameValues[1].Split(",");
+                foreach (var gems in gemsShown)
+                {
+                    string auxGems = gems.Trim();
+                    string[] values = auxGems.Split(" ");
+                    short amountOfGems = short.Parse(values[0]);
+                    string colorOfGem = values[1];
+                    if (amountOfGems > 12 && colorOfGem == "red")
+                    {
+                        isGameImpossible = true;
+                        break;
+                    }
+                    if (amountOfGems > 13 && colorOfGem == "green")
+                    {
+                        isGameImpossible = true;
+                        break;
+                    }
+                    if (amountOfGems > 14 && colorOfGem == "blue")
+                    {
+                        isGameImpossible = true;
+                        break;
+                    }
+                }
+                if (isGameImpossible)
+                    impossibleGames.Add(i + 1);
+            }
+            Console.WriteLine("Total Sum based on possible games " + possibleGames.Except(impossibleGames).Sum());
+        }
+    }
+}
